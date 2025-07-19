fc11e95d7340d5d2c46ad60ff8d369cb2e1ca3b7
Sweta1408 <sweta.singh735@gmail.com>
2025-07-19 16:40:47 +0530

    Palindrome and fibo are added

---
+++ Palindrome.py
@@ -0,0 +1,8 @@
+s = "madam"
+print(s == s[::-1])  # Output: True
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
